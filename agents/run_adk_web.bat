19c45702ad8aa8f60c70c3c41cd7bc744c985806
Bhaskar <bhaskarv.online@gmail.com>
2025-08-10 16:55:17 +0530

    Adding bat file used for starting the ADK Web

---
+++ agents/run_adk_web.bat
@@ -0,0 +1,6 @@
+echo off
+set GOOGLE_GENAI_USE_VERTEXAI=FALSE
+set GOOGLE_API_KEY=REPLACE_WITH_GOOGLE_API_KEY
+
+echo on
+mvn compile exec:java -Dexec.args="--server.port=8082 -adk.agents.source-dir=src\main\java\org\nvision\learn\agents\sample_agents --logging.level.com.google.adk.dev=TRACE --logging.level.com.google.adk.demo.agents=TRACE"
